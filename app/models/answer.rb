6c2d212fb863de142e60b1ec3cdd48eb642ce643
Kanji Hatamoto <kanjihtmt@gmail.com>
2015-07-18 16:49:42 +0900

    ポリモーフィック関連のバグを修正

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,6 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :answerer, class_name: 'User'
   belongs_to :question
-  has_many :votes, as: votable, dependent: :destroy
-  has_many :comments, as: commentable, dependent: :destroy
+  has_many :votes, as: :votable, dependent: :destroy
+  has_many :comments, as: :commentable, dependent: :destroy
 end
