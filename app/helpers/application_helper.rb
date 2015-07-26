97b7925dd78c2131b0fe550e00557684a9e34313
Kanji Hatamoto <kanjihtmt@gmail.com>
2015-07-26 12:50:21 +0900

    リファクタリング: ヘッダーのリンクをアクティブにする処理をヘルパーにまとめた

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,12 @@ module ApplicationHelper
     render = Redcarpet::Render::HTML.new hard_wrap: true
     Redcarpet::Markdown.new(render, options).render text
   end
+
+  def active_headerlink(controller_name, action_name)
+    begin
+      ACTIVE_HEADER_LINKS[controller_name.to_sym][action_name.to_sym]
+    rescue NoMethodError
+      nil
+    end
+  end
 end
---
+++ config/initializers/sof.rb
@@ -0,0 +1,14 @@
+ACTIVE_HEADER_LINKS = {
+  questions: {
+    edit: 'ask_question',
+    new: 'ask_question',
+    index: 'questions',
+    show: 'questions'
+  },
+  users: {
+    index: 'users'
+  },
+  tags: {
+    index: 'tags'
+  }
+}
