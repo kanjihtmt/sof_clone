1640327327188a68cd2edc30b4c68863957567c6
Kanji Hatamoto <kanjihtmt@gmail.com>
2015-07-30 06:22:04 +0900

    userモデルの記述をrails style guideの宣言順に修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniautha１ble
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  include Gravtastic
 
   has_many :questions, foreign_key: :questioner_id
   has_many :answers, foreign_key: :answerer_id
   has_many :comments, foreign_key: :commenter_id, dependent: :destroy
   has_many :votes, dependent: :destroy
+
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniautha１ble
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  gravtastic
 end
