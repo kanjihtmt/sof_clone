0278e0006952597e7a742f85b32ebf0c19968b7a
Kanji Hatamoto <kanjihtmt@gmail.com>
2015-08-10 07:54:15 +0900

    これまで質問一覧をトップページの一覧と適用していたが、専用のレイアウトを作成

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -14,4 +14,16 @@ module QuestionHelper
     return true if controller == 'answers'
     false
   end
+
+  def status(question = nil)
+    return 'unanswered' if !question.is_a? Question
+
+    if question.answers_count > 0 && question.best_answer_id
+      'answered-accepted'
+    elsif question.answers_count > 0
+      'answered'
+    else
+      'unanswered'
+    end
+  end
 end
