0e7289168216626924b0c156ab77a74dbb2c47ed
Kanji Hatamoto <kanjihtmt@gmail.com>
2015-08-11 09:18:07 +0900

    ユーザ一覧を実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,15 @@
 class UsersController < ApplicationController
-  def index
+  before_action :set_user, only: %i(show)
 
+  def index
+    @users = User.search(name_cont: params[:keyword]).result
+    render partial: 'users' if request.xhr?
   end
 
   def show
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
