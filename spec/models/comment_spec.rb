de918c848e8a5943136e500de122e25a953d77bd
Kanji Hatamoto <kanjihtmt@gmail.com>
2015-08-16 07:48:45 +0900

    コメントモデルのテストを実装

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
-RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Comment do
+  it "はbody, commentable_id, commentable_type, commenter_idが入力されていれば有効であること" do
+    comment = Comment.new(
+      body: 'コメントです。',
+      commentable_id: 1,
+      commentable_type: 'Question',
+      commenter_id: 1)
+    expect(comment).to be_valid
+  end
+
+  it "は本文がなければ無効であること" do
+    comment = Comment.new(body: nil)
+    comment.valid?
+    expect(comment.errors[:body]).to include("を入力してください。")
+  end
+
+  it "はcommentable_idの入力がなければ無効であること" do
+    comment = Comment.new(commentable_id: nil)
+    comment.valid?
+    expect(comment.errors[:commentable_id]).to include("を入力してください。")
+  end
+
+  it "はcommentable_typeの入力がなければ無効であること" do
+    comment = Comment.new(commentable_type: nil)
+    comment.valid?
+    expect(comment.errors[:commentable_type]).to include("を入力してください。")
+  end
+
+  it "はコメントした人がいなければ無効であること" do
+    comment = Comment.new(commenter_id: nil)
+    comment.valid?
+    expect(comment.errors[:commenter_id]).to include("を入力してください。")
+  end
 end
