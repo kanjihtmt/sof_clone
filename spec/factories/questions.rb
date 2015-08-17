38ef9cc3344f01da62a33800adff23c7a2abad7d
Kanji Hatamoto <kanjihtmt@gmail.com>
2015-08-18 05:03:09 +0900

    Questionモデルのテストを実装

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -4,8 +4,5 @@ FactoryGirl.define do
     body "質問です。質問です。質問です。質問です。質問です。\n質問です。質問です。質問です。質問です。質問です。\n質問です。質問です。質問です。質問です。質問です。\n"
     questioner_id 1
     tag_list 'rails,ruby'
-    before(:build) do |question|
-      question.tags << FactoryGirl.build(:tag)
-    end
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,56 @@
 require 'rails_helper'
 
 describe Question do
-  it '質問はタイトルが必須です.' do
-    question = create(:question)
-    p Question.find(1)
+  it 'はタイトル、本文、質問者、タグの入力があれば有効であること' do
+    question = build(:question)
+    expect(question).to be_valid
+  end
+
+  it 'はタイトルがなければ無効であること' do
+    question = build(:question, title: nil)
+    question.valid?
+    expect(question.errors[:title]).to include("を入力してください。")
+  end
+
+  it 'はタイトルが8文字以上でなければ無効であること' do
+    question = build(:question, title: '１２３４５６７')
+    question.valid?
+    expect(question.errors[:title]).to include("は8文字以上で入力してください。")
+  end
+
+  it 'は本文がなければ無効であること' do
+    question = build(:question, body: nil)
+    question.valid?
+    expect(question.errors[:body]).to include("を入力してください。")
+  end
+
+  it 'は本文が20文字以上でなければ無効であること' do
+    question = build(:question, body: '１２３４５６７８９０１２３４５６７８９')
+    question.valid?
+    expect(question.errors[:body]).to include("は20文字以上で入力してください。")
+  end
+
+  it 'は質問者の関連がなければ無効であること' do
+    question = build(:question, questioner_id: nil)
+    question.valid?
+    expect(question.errors[:questioner_id]).to include("を入力してください。")
+  end
+
+  it 'は1つ以上のタグがなければ無効であること' do
+    question = build(:question, tag_list: nil)
+    question.valid?
+    expect(question.errors[:tag_list]).to include("少なくとも1つ以上のタグを入力してください。")
+  end
+
+  it 'は保存前にtagsに関連が入っていること' do
+    question = build(:question)
+    question.save!
+    expect(question.tags.map(&:title)).to eq ['rails', 'ruby']
+  end
+
+  it 'は検索時にtag_listを設定すること' do
+    create(:question)
+    question = Question.find_by(title: "質問テストタイトル")
+    expect(question.tag_list).to eq 'rails,ruby'
   end
 end
