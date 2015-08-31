9d56b13054a2698c22b69b24b1ec682d0563658b
kanji <kanjihtmt@gmail.com>
2015-08-31 03:35:51 +0000

    AnswersControllerのテストを実装

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -1,10 +1,5 @@
 require 'rails_helper'
 
-#preview_question_answers POST   /questions/:question_id/answers/preview(.:format) answers#preview
-#        question_answers POST   /questions/:question_id/answers(.:format)         answers#create
-#   edit_question_answers GET    /questions/:question_id/answers/edit(.:format)    answers#edit
-#                         PATCH  /questions/:question_id/answers(.:format)         answers#update
-#                         PUT    /questions/:question_id/answers(.:format)         answers#update
 #                         DELETE /questions/:question_id/answers(.:format)         answers#destroy
 
 describe AnswersController do
@@ -37,41 +32,110 @@ describe AnswersController do
 
   describe 'PATCH #update' do
     context '回答の更新に成功した場合' do
-      it 'データが更新されていること'
-      it '質問詳細ページにリダレクトされていること'
-      it '質問詳細ページ更新完了メッセージが表示されていること'
+      it 'データが更新されていること' do
+        patch :update, question_id: @question, id: @answer,
+          answer: {body: '良く確認して下さい!しっかりエラーをよくみましょう！'}
+        @answer.reload
+        expect(@answer.body).to eq '良く確認して下さい!しっかりエラーをよくみましょう！'
+      end
+      it '質問詳細ページにリダレクトされていること' do
+        patch :update, question_id: @question, id: @answer,
+          answer: {body: '良く確認して下さい!しっかりエラーをよくみましょう！'}
+        expect(response).to redirect_to question_url(@question)
+      end
+      it '質問詳細ページ更新完了メッセージが表示されていること' do
+        patch :update, question_id: @question, id: @answer,
+          answer: {body: '良く確認して下さい!しっかりエラーをよくみましょう！'}
+        expect(flash[:notice]).to be_present
+      end
     end
 
     context '回答の更新に失敗した場合' do
-      it 'データは更新されていないこと'
-      it '再度編集画面が表示されていること'
+      it 'データは更新されていないこと' do
+        patch :update, question_id: @question, id: @answer,
+          answer: {body: 'バリデーションが通らない短い回答'}
+        @answer.reload
+        expect(@answer.body).not_to eq 'バリデーションが通らない短い回答'
+      end
+      it '再度編集画面が表示されていること' do
+        patch :update, question_id: @question, id: @answer,
+          answer: {body: 'バリデーションが通らない短い回答'}
+        expect(response).to render_template('edit')
+      end
     end
   end
 
   describe 'POST #create' do
     context '回答の新規登録に成功した場合' do
-      it 'データが登録されていること'
-      it '質問詳細ページにリダレクトされていること'
-      it '質問詳細ページに登録完了メッセージが表示されていること'
+      it 'データが登録されていること' do
+        expect {
+          post :create, question_id: @question,
+            answer: {body: '良く確認して下さい!しっかりエラーをよくみましょう！'}
+        }.to change(Answer, :count).by(1)
+      end
+      it '質問詳細ページにリダレクトされていること ' do
+        post :create, question_id: @question,
+          answer: {body: '良く確認して下さい!しっかりエラーをよくみましょう！'}
+        expect(response).to redirect_to question_url(@question)
+      end
+      it '質問詳細ページに登録完了メッセージが表示されていること' do
+        post :create, question_id: @question,
+          answer: {body: '良く確認して下さい!しっかりエラーをよくみましょう！'}
+        expect(flash[:notice]).to be_present
+      end
     end
 
     context '回答の新規登録に失敗した場合' do
-      it 'データは登録されていないこと'
-      it '質問詳細画面が表示されていること'
+      it 'データは登録されていないこと' do
+        expect {
+          post :create, question_id: @question,
+            answer: {body: 'バリデーションが通らない短い回答'}
+        }.to change(Answer, :count).by(0)
+      end
+      it '質問詳細画面が表示されていること' do
+        post :create, question_id: @question,
+          answer: {body: 'バリデーションが通らない短い回答'}
+        expect(response).to render_template('questions/show')
+      end
     end
   end
 
   describe 'DELETE #destroy' do
-    context '自分の回答を削除する場合' do
-      it '回答が削除されていること'
-      it '質問詳細ページにリダイレクトされていること'
-      it '質問詳細ページに削除完了メッセージが表示されていること'
+    before do
+      user = build(:user)
+      @answer_of_loginuser = create(:answer, answerer: user, question: @question)
     end
 
     context '自分の回答を削除する場合' do
-      it '回答が削除されていないこと'
-      it '質問詳細ページにリダイレクトされていること'
-      it '質問詳細ページにエラー表示がされていること'
+      it '回答が削除されていること' do
+        expect {
+          delete :destroy, question_id: @question, id: @answer_of_loginuser
+        }.to change(Answer, :count).by(-1)
+      end
+      it '質問詳細ページにリダイレクトされていること' do
+        delete :destroy, question_id: @question, id: @answer_of_loginuser
+        expect(response).to redirect_to(question_url(@question))
+      end
+      it '質問詳細ページに削除完了メッセージが表示されていること' do
+        delete :destroy, question_id: @question, id: @answer_of_loginuser
+        expect(flash[:notice]).to be_present
+      end
+    end
+
+    context '自分の回答を削除する場合' do
+      it '回答が削除されていないこと' do
+        expect {
+          delete :destroy, question_id: @question, id: @answer
+        }.not_to change(Answer, :count)
+      end
+      it '質問詳細ページにリダイレクトされていること' do
+        delete :destroy, question_id: @question, id: @answer
+        expect(response).to redirect_to(question_url(@question))
+      end
+      it '質問詳細ページにエラー表示がされていること' do
+        delete :destroy, question_id: @question, id: @answer
+        expect(flash[:alert]).to be_present
+      end
     end
   end
 end
