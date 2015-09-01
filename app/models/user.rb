fc1d2670e06d2b7315db768c26e7ed4ca45e0008
kanji <kanjihtmt@gmail.com>
2015-09-01 07:51:41 +0000

    不要なスペースや空行を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,8 +6,6 @@ class User < ActiveRecord::Base
   has_many :comments, foreign_key: :commenter_id, dependent: :destroy
   has_many :votes, dependent: :destroy
 
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniautha１ble
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
