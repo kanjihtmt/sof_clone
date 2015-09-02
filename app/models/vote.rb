363b47a7f866e3cb4a8024ece36e2f4beca96e28
kanji <kanjihtmt@gmail.com>
2015-09-02 08:17:57 +0000

    投票を取り消すことが出来るように修正

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,5 +3,17 @@ class Vote < ActiveRecord::Base
   belongs_to :votable, polymorphic: true
 
   validates :value, :votable_id, :votable_type, :user_id, presence: true
-  validates :user_id, uniqueness: { scope: [:votable_id, :votable_type], message: 'は2回以上行うことはできません' }
+  validates :user_id, uniqueness: {
+    scope: [:votable_id, :votable_type, :value],
+    message: 'は2回以上行うことはできません' }
+
+  after_save :clear_vote, on: %i(create)
+
+  protected
+    def clear_vote
+      values = Vote.where(votable_type: self.votable_type, votable_id: self.votable_id,
+                  user_id: self.user_id).map(&:value)
+      Vote.delete_all(votable_type: self.votable_type,
+        votable_id: self.votable_id, user_id: self.user_id) if values.inject(:+) == 0
+    end
 end
--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -30,11 +30,57 @@ describe Vote do
     expect(vote.errors[:user_id]).to include("を入力してください。")
   end
 
-  it "同じユーザが2回投票することができないこと" do
-    user = create(:user)
-    Vote.create(value: 1, votable_type: 'Question', votable_id: 1, user: user)
-    vote = Vote.new(value: -1, votable_type: 'Question', votable_id: 1, user: user)
-    vote.valid?
-    expect(vote.errors[:user_id]).to include("は2回以上行うことはできません")
+  describe '連続投票' do
+    before do
+      @user = create(:user)
+    end
+
+    context '同じユーザが2回連続投票' do
+      it "up投票を連続投票できないこと" do
+        create(:vote, value: 1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote = build(:vote, value: 1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote.valid?
+        expect(vote.errors[:user_id]).to include("は2回以上行うことはできません")
+      end
+
+      it "down投票を連続投票できないこと" do
+        create(:vote, value: -1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote = build(:vote, value: -1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote.valid?
+        expect(vote.errors[:user_id]).to include("は2回以上行うことはできません")
+      end
+    end
+
+    context '既に投票したものと逆の投票を行う' do
+      it 'up投票の次にdown投票を行い、投票を取り消すことができること' do
+        create(:vote, value: 1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote = build(:vote, value: -1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote.valid?
+        expect(vote).to be_valid
+      end
+
+      it 'up投票の次にdown投票を行った後、投票はゼロにクリアされていること' do
+        create(:vote, value: 1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote = build(:vote, value: -1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote.save!
+        count = Vote.where(votable_type: 'Question', votable_id: 1, user: @user).count
+        expect(count).to be 0
+      end
+
+      it 'down投票の次にup投票を行い、投票を取り消すことができること' do
+        create(:vote, value: -1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote = build(:vote, value: 1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote.valid?
+        expect(vote).to be_valid
+      end
+
+      it 'down投票の次にup投票を行った後、投票はゼロにクリアされていること' do
+        create(:vote, value: -1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote = build(:vote, value: 1, votable_type: 'Question', votable_id: 1, user: @user)
+        vote.save!
+        count = Vote.where(votable_type: 'Question', votable_id: 1, user: @user).count
+        expect(count).to be 0
+      end
+    end
   end
 end
