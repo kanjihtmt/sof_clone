6ed4edcb6ccb7911c8c5844e27dd87a131fd54ab
regonn <github@regonn.tokyo>
2015-09-09 18:22:52 +0900

    refactor vote model

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -16,4 +16,17 @@ class Vote < ActiveRecord::Base
       Vote.delete_all(votable_type: self.votable_type,
         votable_id: self.votable_id, user_id: self.user_id) if values.inject(:+) == 0
     end
+    # clear_vote の中に条件ロジックが含まれてしまっていて、clear_voteという名前以外のことをしてしまっている気がするので
+    # after_saveの所を
+    # after_save :clear_vote, on: %i(create), if: :canceled_vote?
+    # として
+    # def canceled_vote?
+    #   Vote.where(votable_type: self.votable_type, votable_id: self.votable_id, user_id: self.user_id).pluck(&:value).inject(:+) == 0
+    # end
+    #
+    # def clear_vote
+    #   Vote.delete_all(votable_type: self.votable_type, votable_id: self.votable_id, user_id: self.user_id)
+    # end
+    # とした方が条件とclear_voteのロジックが分かれて理解しやすくなると思います。
+    # 途中の所でmapをpluckに変えているのはVoteのObjectを生成するかしないかの違いで無駄なObjectは生成しないようにしています。
 end
